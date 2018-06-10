fc15c42c4f019890103d1523305e065d839f5bc1
Caitlin Foley <Kitty@Caitlins-Air.fritz.box>
2018-06-10 21:09:19 +1200

    initial commit

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+
+    public int salary;
+    public Double serviceLength;
+
+    public Employee(){
+        salary = 80000;
+        serviceLength = 2.5; 
+
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello"); 
+        Person person1 = new Person();
+        Employee employee1 = new Employee();
+        System.out.println(person1.name);
+        System.out.println(employee1.salary);
+    }
+
+}
